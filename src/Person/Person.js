fdda0f9b09286e28c39d28b6dfc2a2db5d11a36b
samalo <smhonds@gmail.com>
2018-10-13 05:30:10 +0200

    Update src/Person/Person.js

---
+++ src/Person/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Person.css';
+
+const person = ( props ) => {
+
+return(
+
+  <div className="Person">
+  <p onClick={props.click}>I'm a {props.name } and i'm {props.age} years old</p>
+  <p>{props.children}</p>
+  <input type="text" onChange={props.changed} value={props.name}/>
+</div>
+)
+ 
+  
+}
+
+export default person;
